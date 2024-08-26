70388bc2616cb82edd8f5b1fcb327cea54f97fb1
StefanRammo <stefanrammo85@gmail.com>
2024-08-26 11:36:13 +0300

    init

---
+++ ContosoUniTTHK/ContosoUniTTHK/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContosoUniTTHK.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
