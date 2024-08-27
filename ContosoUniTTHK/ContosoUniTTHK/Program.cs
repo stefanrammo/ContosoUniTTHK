8ce538128765270688546e04f2f4dd3b7b866357
StefanRammo <stefanrammo85@gmail.com>
2024-08-27 11:11:03 +0300

    CreateDbIfNotExists

--- ContosoUniTTHK/ContosoUniTTHK/Program.cs
+++ ContosoUniTTHK/ContosoUniTTHK/Program.cs
@@ -11,20 +11,7 @@ namespace ContosoUniTTHK
         public static void Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<SchoolContext>();
-                    DbInitializer.Initialize(context);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
-            }
+            CreateDbIfNotExists(host);
             host.Run();
 
             var builder = WebApplication.CreateBuilder(args);
